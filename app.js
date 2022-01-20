ebe606dfa6da0db2ecb8e0963f082e534674ce82
jayramani8 <70213053+jayramani8@users.noreply.github.com>
2022-01-20 23:41:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
-var ChekIn;
-var CheckOut;
-var today;
-var today1, calculateInTime = 0, calculateBreakTime = 0;
+let ChekIn;
+let CheckOut;
+let today;
+let today1, calculateInTime = 0, calculateBreakTime = 0;
 document.getElementById('chekOut').disabled = true;
 
 document.getElementById('chekIn').addEventListener('click', (event) => {
@@ -13,15 +12,14 @@ document.getElementById('chekIn').addEventListener('click', (event) => {
     today = new Date();
     ChekIn = today.getHours() + ":" + today.getMinutes();
 
-    var table = document.getElementById("myTable");
 
-    var BreakDiffInTime = (today1.getTime() / 1000).toFixed(0);
-    var BreakDiffOutTime = (today.getTime() / 1000).toFixed(0);
+    let BreakDiffInTime = (today1.getTime() / 1000).toFixed(0);
+    let BreakDiffOutTime = (today.getTime() / 1000).toFixed(0);
 
-    var brakHours = Math.floor(((BreakDiffOutTime - BreakDiffInTime) / 3600) % 24);
-    var brakMinutes = Math.floor((((BreakDiffOutTime - BreakDiffInTime) / 60) % 60));
+    let brakHours = Math.floor(((BreakDiffOutTime - BreakDiffInTime) / 3600) % 24);
+    let brakMinutes = Math.floor((((BreakDiffOutTime - BreakDiffInTime) / 60) % 60));
 
-    var totalBreakTime = (brakHours * 60) + brakMinutes;
+    let totalBreakTime = (brakHours * 60) + brakMinutes;
     calculateBreakTime = calculateBreakTime + totalBreakTime;
 
 
@@ -35,19 +33,19 @@ document.getElementById('chekOut').addEventListener('click', (event) => {
     today1 = new Date();
     CheckOut = today1.getHours() + ":" + today1.getMinutes();
 
-    var table = document.getElementById("myTable");
+    let table = document.getElementById("myTable");
 
-    var CheckInTime = (today.getTime() / 1000).toFixed(0);
-    var CheckOutTime = (today1.getTime() / 1000).toFixed(0);
+    let CheckInTime = (today.getTime() / 1000).toFixed(0);
+    let CheckOutTime = (today1.getTime() / 1000).toFixed(0);
 
-    var inHours = Math.floor(((CheckOutTime - CheckInTime) / 3600) % 24);
-    var inMinuts = Math.floor((((CheckOutTime - CheckInTime) / 60) % 60));
+    let inHours = Math.floor(((CheckOutTime - CheckInTime) / 3600) % 24);
+    let inMinuts = Math.floor((((CheckOutTime - CheckInTime) / 60) % 60));
 
     inHours = inHours < 10 ? '0' + inHours : inHours;
     inMinuts = inMinuts < 10 ? '0' + inMinuts : inMinuts;
 
-    var totalTime = inHours + ":" + inMinuts;
-    var TotalIntime = (inHours * 60) + inMinuts;
+    let totalTime = inHours + ":" + inMinuts;
+    let TotalIntime = (inHours * 60) + inMinuts;
 
     calculateInTime = calculateInTime + TotalIntime;
     // console.log(totalTime);
@@ -64,8 +62,8 @@ document.getElementById('chekOut').addEventListener('click', (event) => {
 document.getElementById('totalTime').addEventListener('click', (event) => {
     event.preventDefault();
 
-    var h = Math.floor(calculateInTime / 60);
-    var m = calculateInTime % 60;
+    let h = Math.floor(calculateInTime / 60);
+    let m = calculateInTime % 60;
     h = h < 10 ? '0' + h : h;
     m = m < 10 ? '0' + m : m;
     document.getElementById("show-total-intime").innerHTML = (h + ":" + m);
@@ -75,8 +73,8 @@ document.getElementById('totalTime').addEventListener('click', (event) => {
 document.getElementById('totalBreak').addEventListener('click', (event) => {
     event.preventDefault();
 
-    var h = Math.floor(calculateBreakTime / 60);
-    var m = calculateBreakTime % 60;
+    let h = Math.floor(calculateBreakTime / 60);
+    let m = calculateBreakTime % 60;
     h = h < 10 ? '0' + h : h;
     m = m < 10 ? '0' + m : m;
     document.getElementById("show-total-break").innerHTML = (h + ":" + m);
